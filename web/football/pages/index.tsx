08a3e5412d02fc7e1e3334ceb934fd00a6440b9b
Parham Alvani <parham.alvani@gmail.com>
2021-11-19 13:22:19 +0330

    fix: correct homepage

--- web/football/pages/index.tsx
+++ web/football/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 import Rotation from "../components/Rotation";
@@ -8,11 +7,14 @@ import Container from "@mui/material/Container";
 
 const Home: NextPage = () => {
   return (
-    <Container maxWidth="lg">
-      <h1>Football</h1>
-      <h4>Rotate the camera at your match</h4>
-      <Rotation />
-    </Container>
+    <>
+      <Head>Football</Head>
+      <Container maxWidth="lg">
+        <h1>Football</h1>
+        <h4>Rotate the camera at your match</h4>
+        <Rotation />
+      </Container>
+    </>
   );
 };
 
